4a40bf908f56b864d9cbc04b9014809f02eac88e
brunowebertyzup <82046327+brunowebertyzup@users.noreply.github.com>
2021-05-21 17:31:10 -0300

    v006-exibir-lista-livros

--- casadocodigo/src/main/java/br/com/zupacademy/brunoweberty/casadocodigo/livro/LivroController.java
+++ casadocodigo/src/main/java/br/com/zupacademy/brunoweberty/casadocodigo/livro/LivroController.java
@@ -6,7 +6,12 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +33,12 @@ public class LivroController {
 		Livro livro = request.converterParaLivro(manager);
 		livroRepository.save(livro);
 		return ResponseEntity.ok(request.toString());
+	}
+	
+	@GetMapping
+	public Page<LivroResponse> lista(@PageableDefault(sort= "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao) {
+			Page<Livro> livros = livroRepository.findAll(paginacao);
+			return LivroResponse.converter(livros);
 	}
 	
 }
